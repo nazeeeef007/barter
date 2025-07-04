83bda2d3f60944a550c9aa45451f432dbc962115
nazeef <muhdnazeef007@gmail.com>
2025-07-04 17:30:46 +0800

    bryg

--- backend/src/main/java/com/barter/backend/config/CorsConfig.java
+++ backend/src/main/java/com/barter/backend/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));  // your frontend
+        config.setAllowedOrigins(List.of("http://localhost:5173",  "https://barter2.netlify.app"));  // your frontend
         config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
